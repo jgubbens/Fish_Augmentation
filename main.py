866a4f28b6e6c451d151616531775621bd12d801
jgubbens <146300159+jgubbens@users.noreply.github.com>
2024-12-02 13:55:46 +0800

    elastic deformation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ObjectPlacement.sam2_generator import ImageSegmenter
 from ObjectPlacement.brightness_modifier import BrightnessModifier
 from ObjectPlacement.object_placement import ObjectPlacer
 from DiffuseMix.diffusemix import Diffuse
+from ElasticDeformation.elastic_deformation import ElasticDeformer
 import os
 
 # ARGS - WILL BE GOTTEN FROM CONFIG FILE IN THE FUTURE
@@ -16,6 +17,11 @@ def main():
     modifier = BrightnessModifier(source_coords, adj_intensity)
     brightness = modifier.find_brightness((0.8, 0.2), (0.6, 0.6))
     print(brightness)
+
+    # Elastic Deformation
+    deformer = ElasticDeformer(30, 5)
+    deformer.run_transform('ElasticDeformation/original.png')
+    deformer.transform_dir(training_path)
     
     # Run SAM2
     model_checkpoint = 'checkpoints/sam2.1_hiera_large.pt'
@@ -23,8 +29,8 @@ def main():
     segmenter = ImageSegmenter(model_checkpoint, model_config)
     images_folder = os.path.join(training_path, 'images')
     labels_folder = os.path.join(training_path, 'labels')
-    segmenter.process_all_files(images_folder, labels_folder)
-    segmenter.copy_labels_with_full_image_bbox(labels_folder, 'segmented/bbox_labels', images_folder)
+    #segmenter.process_all_files(images_folder, labels_folder)
+    #segmenter.copy_labels_with_full_image_bbox(labels_folder, 'segmented/bbox_labels', images_folder)
     
     # Run DiffuseMix
     diffuse_output = 'result'
@@ -32,13 +38,13 @@ def main():
     diffuse_train_data = 'segmented'
     prompts = 'Ukiyo-e,Snowy,Watercolor'
     diffuse_runner = Diffuse(diffuse_output, diffuse_train_data, prompts)
-    diffuse_runner.run_diffusemix()
-    diffuse_runner.resize_output('diffuse-output')
+    #diffuse_runner.run_diffusemix()
+    #diffuse_runner.resize_output('diffuse-output')
 
     # Run Object Placement
     segmented_dir = os.path.join('diffuse-output', 'images')
     label_dir = os.path.join('diffuse-output', 'labels')
-    random_placer = ObjectPlacer(background_image, num_generated, modifier, segmented_dir, label_dir)
-    random_placer.generate_augmented_images()
+    #random_placer = ObjectPlacer(background_image, num_generated, modifier, segmented_dir, label_dir)
+    #random_placer.generate_augmented_images()
 if __name__ == "__main__":
     main()
